c33401ce5f6ecd339d2adfab53b778adcada2c64
timelessover <shanpaodaxia@qq.com>
2019-04-08 21:13:43 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+module.exports = {
+    entry: {
+        main: __dirname + '/src/main.js'       //入口
+    },
+    output: {
+        filename: 'bundle.js',               //输出的文件名
+        path: path.resolve(__dirname, 'build') //输出文件所在的目录
+    },
+    devServer: { // 检测代码变化并自动重新编译并自动刷新浏览器
+        contentBase: path.resolve(__dirname, 'build') // 设置静态资源的根目录
+    },
+    module: { // 如何处理项目中不同类型的模块
+        rules: [ // 用于规定在不同模块被创建时如何处理模块的规则数组
+            {
+                test: /(\.js)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "env"
+                        ]
+                    }
+                },
+                exclude: path.resolve(__dirname, 'node_modules'),
+                include: path.resolve(__dirname, 'src'),
+            }
+        ]
+    }
+}
